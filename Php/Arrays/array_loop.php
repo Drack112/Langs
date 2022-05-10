fbecc9b6b011dc7cd0ef3b5c5b877ea2c771eb5e
drack112 <onepessoa3@gmail.com>
2022-05-10 17:01:30 -0300

    Basic PHP Finished

--- Php/Arrays/array_loop.php
+++ Php/Arrays/array_loop.php
@@ -1,8 +1,8 @@
 <?php
-$cars = array("Volvo", "BLW", "Toyota");
+$cars = array("Volvo", "BLW", "toyota");
 $arrlen = count($cars);
 
-for ($x = 0; $x < $arrlen; $x++) {
+for ($x = 0, $x < $arrlen; $x++;) {
     echo "$cars[$x]";
 }
 ;
---
+++ Php/Arrays/rsort.php
@@ -0,0 +1,5 @@
+<?php
+$numbers = array(4, 6, 2, 22, 11);
+rsort($numbers);
+echo $numbers[4];
+?>
